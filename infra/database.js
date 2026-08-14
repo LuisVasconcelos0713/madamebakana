dbbb50d16734e51aea07fc68de64f9645779f25a
Luis Gustavo Vasconcelos <83058118+LuisVasconcelos0713@users.noreply.github.com>
2026-08-14 21:42:23 +0000

    feat: adding database and env files

---
+++ infra/database.js
@@ -0,0 +1,31 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  ssl: true,
+});
+
+const query = async (queryObject) => {
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+  });
+  try {
+    const result = await pool.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export default {
+  query: query,
+};
