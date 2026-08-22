e91e790a735520d7cc1ccdfd5d9d84460172bcd8
LuisVasconcelos0713 <Lgvs1307@gmail.com>
2026-08-21 21:22:55 -0300

    fix import prisma on deleteproduct.js

--- app/actions/DeleteProduct.js
+++ app/actions/DeleteProduct.js
@@ -1,6 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import prisma from "../../lib/prisma";
 
 export async function DeleteProduct(id) {
   const product = await prisma.product.delete({
