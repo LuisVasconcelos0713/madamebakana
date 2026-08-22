5b0296bdc0cd37ec6e0492044caf55fc2709b587
LuisVasconcelos0713 <Lgvs1307@gmail.com>
2026-08-21 21:05:45 -0300

    fixing proxy.js bug

--- proxy.js
+++ proxy.js
@@ -1,25 +1,4 @@
-import { NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
-
-export async function proxy(req) {
-  const { pathname } = req.nextUrl;
-
-  // Se a rota for /admin, verifica o token diretamente dos cookies
-  if (pathname.startsWith("/admin")) {
-    const token = await getToken({
-      req,
-      secret: process.env.AUTH_SECRET,
-    });
-
-    // Se NÃO houver token válido, redireciona para o login
-    if (!token) {
-      const loginUrl = new URL("/login", req.url);
-      return NextResponse.redirect(loginUrl);
-    }
-  }
-
-  return NextResponse.next();
-}
+export { auth as proxy } from "./auth";
 
 export const config = {
   matcher: ["/admin/:path*", "/admin"],
